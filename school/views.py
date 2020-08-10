24d899ed6760f9a7d912b3ea705a4c009c06954b
Ivan <oiv@live.ru>
2020-08-10 23:30:32 +0500

    v 0.1
    Исправлены модели
    Теперь у одного ученика может быть несколько учителей
    Созданы миграции
    Фикстуры для новых моделей сохранены в School2.json

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    object_list = Student.objects.all()
+    context = {'object_list': object_list}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
